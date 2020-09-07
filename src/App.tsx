d9e49eb700a14ac3ef90035184dd8ba726016bd4
motokikasai <kmomoky@hotmail.com>
2020-09-07 22:09:34 +0200

    Apply toggle for checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Grid } from '@material-ui/core';
 import Header from './components/Header';
 import TodoListItem from './components/TodoListItem';
 
-const todos: Array<Todo> = [
-  { text: 'Nail TS', complete: false },
-  { text: 'Manage time', complete: true },
+const initialTodos: Array<Todo> = [
+  { text: 'Nail TS', complete: true },
+  { text: 'Manage time', complete: false },
 ];
 
-function App() {
+const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo: ToggleTodo = selectedTodo => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <div className='App'>
       <Grid container direction='column'>
@@ -18,7 +33,8 @@ function App() {
         <Grid item container>
           <Grid item xs={false} sm={2} />
           <Grid item xs={10} sm={8}>
-            <TodoListItem todo={todos[0]} />
+            <TodoListItem todo={todos[0]} toggleTodo={toggleTodo} />
+            <TodoListItem todo={todos[1]} toggleTodo={toggleTodo} />
             <Button variant='contained' color='primary'>
               Click me here!
             </Button>
@@ -28,6 +44,6 @@ function App() {
       </Grid>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import './styles/TodoListItem.css';
 
 interface TodoListItemProps {
   todo: Todo;
+  toggleTodo: ToggleTodo;
 }
 
 const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
   return (
     <li>
-      <label>
+      <label className={todo.complete ? 'complete' : undefined}>
         <input type='checkbox' checked={todo.complete} />
         {todo.text}
       </label>
